15b9266572121cd48b4c00df9c023690b702570a
Amiran97 <todua.amiran97@gmail.com>
2017-06-22 15:15:31 +0300

    +

--- application/views/Head.php
+++ application/views/Head.php
@@ -1,6 +1,6 @@
 <style>
 	body{
-		
+		margin: 0;
 		background: url(Ressourse/BackgroundNOVK.jpg) no-repeat;
 		background-size: 100%;
 	}
@@ -10,6 +10,9 @@
     	width: 450px;
 	}
 	div.Head{
+		padding: 1em 0;
+		width: 100%;
+		max-width: 100%;
 		background: linear-gradient(to top left, rgba(72, 61, 139, 0.9), rgba(186, 85, 211, 0.9));
     	border-radius: 0 0 8px 8px;
 	}
@@ -36,12 +39,25 @@
 	input.Enter{
 		width: 80%;
 	}
+	button.Content{
+		width: 150px;
+	}
 	button.RefBtn{
 		padding: 5px;
 		font-family: comic sans ms;
 		border-color:Purple;
 		border-radius: 8px;
 		background-color: Indigo;
+	}
+	button.HeadBtn
+	{
+		margin-left: 50px;
+		padding: 8px;
+		font-family: comic sans ms;
+		border-color:Purple;
+		border-radius: 20px;
+		background-color: Indigo;
+		font-size: 32px;
 	}
 	a{
 		vertical-align: middle;
@@ -67,8 +83,41 @@
 	label.Head{
 		font-size: 32px;
 	}
-
+	div.Content{
+		margin: 30px;
+		background: linear-gradient(to top left, rgba(72, 61, 139, 0.9), rgba(186, 85, 211, 0.9));
+    	border-radius: 8px;
+    	padding: 10px;
+	}
+	th.Content{
+		width: 100%;
+	}
+	dd{
+		margin-right: 0px;
+		margin-top: 15px;
+		margin-left: 50px;
+		width: 150px;
+	}
 </style>
 <div class="Head">
-	
+	<button type="button" class="HeadBtn"><a href="Feed">NOVK</a></button>
 </div>
+<table>
+<tr>
+	<th>
+				<dd>
+					<button type="button" class="RefBtn Content"><a href="Profil">Моя странница</a></button>
+				</dd>
+				<dd>
+					<button type="button" class="RefBtn Content"><a href="Message">Сообщения</a></button>
+				</dd>
+				<dd>
+					<button type="button" class="RefBtn Content"><a href="Friends">Друзья</a></button>
+				</dd>
+	</th>
+	<th class="Content">
+		<div class="Content">
+		</div>
+	</th>
+</tr>
+</table>

ab42bf8e95a8198593eb71608921c295d245324e
Amiran97 <todua.amiran97@gmail.com>
2017-06-22 14:12:50 +0300

    Шапка и главная страница!

---
+++ application/views/Head.php
@@ -0,0 +1,74 @@
+<style>
+	body{
+		
+		background: url(Ressourse/BackgroundNOVK.jpg) no-repeat;
+		background-size: 100%;
+	}
+	div.Login{
+		margin: auto;
+    	margin-top:15%;
+    	width: 450px;
+	}
+	div.Head{
+		background: linear-gradient(to top left, rgba(72, 61, 139, 0.9), rgba(186, 85, 211, 0.9));
+    	border-radius: 0 0 8px 8px;
+	}
+	div.LoginContent{
+    	padding: 10px;
+    	background: linear-gradient(to top left, rgba(72, 61, 139, 0.9), rgba(186, 85, 211, 0.9));
+    	border-radius: 8px;
+	}
+	label{
+		margin: 10px;
+		padding: 5px;
+		color: Violet;
+		font-family: comic sans ms;
+	}
+	input.Inp{
+		margin: 10px;
+		background-color: Indigo;
+		color: Violet;
+		padding: 5px;
+		border-color: Purple;
+		border-radius: 8px;
+		font-family: comic sans ms;
+	}
+	input.Enter{
+		width: 80%;
+	}
+	button.RefBtn{
+		padding: 5px;
+		font-family: comic sans ms;
+		border-color:Purple;
+		border-radius: 8px;
+		background-color: Indigo;
+	}
+	a{
+		vertical-align: middle;
+		text-decoration: none;
+		color: Violet;
+	}
+	table{
+		width: 100%;
+	}
+	th.Head{
+		width: 100px;
+	}
+	div.Rigth{
+		margin-left: 50%;
+	}
+	div.LoginHead{
+		width: 250px;
+		background: linear-gradient(to top left, rgba(72, 61, 139, 0.9), rgba(186, 85, 211, 0.9));
+    	border-radius: 8px 8px 0 0;
+		padding: 10px;
+		text-align: center;
+	}
+	label.Head{
+		font-size: 32px;
+	}
+
+</style>
+<div class="Head">
+	
+</div>
